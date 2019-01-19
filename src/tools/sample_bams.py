2e21b789611e0fb4fbf4cc58c9e69d6b27afc1a7
khzhu <kzhu@uhnresearch.ca>
2019-01-19 11:19:41 -0500

    using multi threads to downsample bams

--- src/tools/sample_bams.py
+++ src/tools/sample_bams.py
@@ -1,41 +1,72 @@
 from __future__ import division
 from multiprocessing import Process
 import subprocess
-from multiprocessing import Pool, TimeoutError
+from multiprocessing import Pool
 import time
 import os
+import sys
 import argparse
-
-def info(title):
-    print (title)
-    print ('module name:', __name__)
-    if hasattr(os, 'getppid'):  # only available on Unix
-        print ('parent process:', os.getppid())
-    print ('process id:', os.getpid())
+import glob
 
 def get_count(bam, max_workers):
-    print ("count total number of reads in %s ..."%bam)
-    cmd = ['sambamba','view','-c','-t',str(max_workers),bam]
+    """
+    Count total number of paired reads for a given bam file
+    :param bam: input bam files
+    :param max_workers: number of threads
+    :return: total paired reads from input bam file
+    """
+    print ("Count total number of paired reads in %s ..."%bam)
+    cmd = ['samtools','view','-c','-f', '3','-@',str(max_workers),bam]
     out, err = subprocess.Popen(cmd, stdin = subprocess.PIPE, stdout=subprocess.PIPE).communicate()
     return int(out.split()[0])
 
 def subset(seed, bam, read, output, count, max_workers):
+    """
+    Down sampling bam file for a given rate
+    :param seed: random sampling rate
+    :param bam: input bam file
+    :param read: dilution read
+    :param output: output path
+    :param count: total number of paired reads of the bam file
+    :param max_workers: number of threads
+    :return: None
+    """
     bam_name = os.path.basename(bam).split(".")[0]
     sample_rate = round((seed + read/count), 8)
     print (sample_rate)
-    output = os.path.join(output,"%s_%s_%s_%s.bam"%(bam_name,seed, str(sample_rate).split(".")[1],read))
-    cmd = 'samtools view -s %s -b %s | samtools sort > %s'%(sample_rate, bam, output) 
+    sorted_bam = os.path.join(output,"%s_%s_%s_%s.bam"%(bam_name,seed, str(sample_rate).split(".")[1],read))
+    cmd = 'samtools view -s %s -f 3 -@ %s -b %s | samtools sort -n > %s'%(sample_rate, max_workers, bam, sorted_bam)
 
     print (cmd)
     if os.system(cmd) == 0:
-        return output
+        bam2fq_cmd = 'bedtools bamtofastq -i %s -fq %s -fq2 %s'%(sorted_bam,
+                                                                 sorted_bam.replace(".bam","-1.fastq"),
+                                                                 sorted_bam.replace(".bam", "-2.fastq"))
+        print (bam2fq_cmd)
+        if os.system(bam2fq_cmd) == 0:
+            return sorted_bam
+        else:
+            print ("Fail to convert sorted bam %s to fastq" %sorted_bam)
+            sys.exit(1)
     else:
-        return None
+        print("Fail to downsample bam %s" %bam)
+        sys.exit(1)
 
 def execute(seed, bams, reads, output, max_workers, counts):
-    print ("total number of reads in the bams: %s" %counts)
+    """
+    Down sampling bam files for given rates and then convert sorted bam files to paired fastq files
+    :param seed: random sampling seed
+    :param bams: input bam files
+    :param reads: comma separated dilution reads
+    :param output: directory to store fastq files
+    :param max_workers: number of concurrent processes
+    :param counts: total number of paired reads in input bam files
+    :return: None
+    """
+    print ("Convert sorted bam to fastq...")
     pool = Pool(processes=max_workers)
-    results = [ pool.apply_async(subset, [int(seed), str(bam), int(read), str(output), count, max_workers]) for bam, read, count in zip(bams,reads, counts) ]
+    results = [ pool.apply_async(subset, [int(seed), str(bam), int(read), str(output), count, max_workers])
+                for bam, read, count in zip(bams,reads, counts) ]
     sub_bams = []
     for async_result in results:
         try:
@@ -46,21 +77,25 @@ def execute(seed, bams, reads, output, max_workers, counts):
         except ValueError as e:
            print(e)
 
-    bam_names = [os.path.basename(one_bam).split(".")[0] for one_bam in sub_bams] 
-    merged_bam = os.path.join(os.path.dirname(sub_bams[0]),"%s.bam"%"_".join(bam_names))
-    merge_cmd = 'java -jar $picard_dir/picard.jar MergeSamFiles I=%s I=%s O=%s'%(sub_bams[0], sub_bams[1], merged_bam)
-    if os.system(merge_cmd) == 0:
-        #delete subset bams
-        for one_bam in sub_bams:
-            os.system("rm -f %s"%one_bam)
+    sorted_bams = glob.glob(os.path.join(output, "%s*.bam"%bams[0].split(".")[0]))
+    sorted_bams_1 = glob.glob(os.path.join(output, "%s*.bam" % bams[1].split(".")[0]))
 
-        bam2fq_cmd = 'bedtools bamtofastq -i %s -fq %s -fq2 %s'%(merged_bam,
-                                                                 merged_bam.replace(".bam","-1.fastq"),
-                                                                 merged_bam.replace(".bam", "-2.fastq"))
-        os.system(bam2fq_cmd)
+    for bam1, bam2 in zip(sorted_bams, sorted_bams_1):
+        fastq_name = "{0}_{1}".format(os.path.basename(bam1).split(".")[0],os.path.basename(bam2).split(".")[1])
+        cat_cmd = "cat {0} {1} > {2}".format(bam1.replace(".bam","-%s.fastq"%seed),
+                                 bam2.replace(".bam","-%s.fastq"%seed),
+                                 os.path.join(output, "%s-%s.fastq" %(fastq_name,seed)))
+        print (cat_cmd)
+        if os.sytem(cat_cmd) != 0:
+            print ("Failed to create fasstq {0}.fastq".format(fastq_name))
+            sys.exit(1)
+    print ("Done!")
 
 def get_options():
-
+    """
+    get input options from command line
+    :return: command line arguments
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--bams", type=str, required=True,
                         help="Comma separated bam files including path")
@@ -74,6 +109,8 @@ def get_options():
                         help="Total reads in final merged bam")
     parser.add_argument("-s", "--seeds", type=str, required=False, default="1,101",
                         help="Range for random sampling, eg. 1,101 is 1-100")
+    parser.add_argument("-del", "--delete", action='store_true',
+                        help="If set to true, all intermediate sorted Bam files will be removed after fastq files generated")
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -83,15 +120,11 @@ if __name__ == '__main__':
     start,end = [ int(x) for x in options.seeds.split(",") ]
     bams = [ str(bam) for bam in options.bams.split(",") ]
     counts = [ get_count(bam, options.jobs) for bam in bams ]
-    # dilutions = [[100000000, 0], [99000000, 1000000],[98000000, 2000000], [97000000, 3000000],
-    #                 [96000000, 4000000], [95000000, 5000000], [94000000, 6000000],[93000000, 7000000],
-    #                 [92000000, 8000000], [91000000, 9000000], [90000000, 10000000], [8000000, 12000000],
-    #                 [6000000, 14000000], [4000000, 16000000],[2000000, 18000000],[0, 100000000]]
     for read in dilutions:
         reads = [read, options.reads - read]
-        print ("reads to be extracted: %s" %reads)
+        print ("Reads need to be extracted: %s" %reads)
         for seed in range(start, end):
-           print ("seed : %s" %seed)
+           print ("Seed : %s" %seed)
            p = Process(target=execute, args=(seed, bams, reads, options.output, options.jobs, counts))
            p.start()
 
